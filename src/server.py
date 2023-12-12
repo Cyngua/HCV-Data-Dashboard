a080cecca7456c49c7fc7db8d573b705a5dc24e4
Xincheng Cai <xincheng.cai@duke.edu>
2023-12-12 15:41:43 -0500

    results basic settings

--- src/server.py
+++ src/server.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import plotly
-import plotly.express as px
 
 # python3 server.py
 df = pd.read_csv('../data/processed_hcvdat.csv')
@@ -18,22 +16,23 @@ def index():
 # data overview
 @app.route('/overview', methods=['GET', 'POST'])
 def overview():
-    # Default number of rows to display
     num_rows = 5
-
     if request.method == 'POST' or request.args.get('rowSelection'):
         num_rows_param = request.form.get('rowSelection') or request.args.get('rowSelection')
         # Display selected number of rows
         num_rows = int(num_rows_param)
-    
     data_for_overview = hcv_data.iloc[:num_rows, :]
     return render_template('overview.html', data = data_for_overview)
 
 # data exploration
 @app.route('/data-exploration', methods=['GET', 'POST'])
 def explore():
-
     return render_template('exploration.html', hcv_data_json = hcv_data_json)
+
+# prediction results
+@app.route('/prediction-results', methods=['GET', 'POST'])
+def results():
+    return render_template('results.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
